9ee30bce883f2d8a5924eee565723e4edd66dfb6
Rahul K <41164942+rk134@users.noreply.github.com>
2021-10-10 12:39:56 +0100

    vince: adapt

--- buildbot.sh
+++ buildbot.sh
@@ -1,12 +1,12 @@
 # Rom building script for CircleCI
 # coded by bruh™ aka Live0verfl0w
 
-MANIFEST_LINK=git://github.com/ProjectSakura/android.git
-BRANCH=10
-ROM_NAME=lineage
-DEVICE_CODENAME=dreamlte
-GITHUB_USER=Exynos-nigg
-GITHUB_EMAIL=vsht700@gmail.com
+MANIFEST_LINK=https://github.com/Project-Fluid/manifest.git
+BRANCH=fluid-12
+ROM_NAME=fluid
+DEVICE_CODENAME=vince
+GITHUB_USER=rk134
+GITHUB_EMAIL=rahul.kantrapally@gmail.com
 WORK_DIR=$(pwd)/${ROM_NAME}
 JOBS=nproc
 
@@ -22,20 +22,21 @@ repo init --depth=1 -u ${MANIFEST_LINK} -b ${BRANCH}
 repo sync --current-branch --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j${JOBS}
 
 # clone device sources
-git clone -b lineage-17.1 https://github.com/Sakura-Devices/device_samsung_dreamlte.git device/samsung/dreamlte 
+rm -rf hardware/qcom-caf/msm8996/audio
+rm -rf hardware/qcom-caf/msm8996/display
+rm -rf hardware/qcom-caf/msm8996/media
+git clone -b A12-fluid-WIP https://github.com/rxhulkxnt44/device_xiaomi_vince.git device/xiaomi/vince
+git clone -b arrow https://github.com/rxhulkxnt44/vendor_xiaomi_vince.git device/xiaomi/vince
+git clone -b arrow https://github.com/rxhulkxnt44/vendor_xiaomi_vince.git vendor/xiaomi/vince
+cd hardware/qcom-caf/msm8996
+git clone -b eleven https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_display.git hardware/qcom-caf/msm8996/display
+git clone -b eleven https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_audio.git hardware/qcom-caf/msm8996/audio
+git clone -b eleven https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_media.git hardware/qcom-caf/msm8996/media
+cd ../../../
 
-git clone -b lineage-17.1 https://github.com/Sakura-Devices/device_samsung_universal8895-common.git device/samsung/universal8895-common 
-
-git clone -b pie https://github.com/Sakura-Devices/kernel_samsung_universal8895.git kernel/samsung/universal8895 
-
-git clone -b lineage-17.1 https://github.com/Sakura-Devices/vendor_samsung_dreamlte.git vendor/samsung/dreamlte 
-
-git clone -b lineage-17.1 https://github.com/Sakura-Devices/vendor_samsung_universal8895-common.git vendor/samsung/universal8895-common 
-
-git clone -b lineage-17.1 https://github.com/LineageOS/android_hardware_samsung.git hardware/samsung
 
 # Start building!
 . build/envsetup.sh
-lunch lineage_${DEVICE_CODENAME}-userdebug
-mka bacon -j${JOBS}
+lunch fluid_${DEVICE_CODENAME}-eng
+make sepolicy
 
