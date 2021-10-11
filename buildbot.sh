f7211bac07189d45d455d1d13bcd3d4419f8c76f
Rahul K <41164942+rk134@users.noreply.github.com>
2021-10-11 17:44:26 +0100

    prep for awaken

--- buildbot.sh
+++ buildbot.sh
@@ -1,14 +1,14 @@
 # Rom building script for CircleCI
 # coded by bruh™ aka Live0verfl0w
 
-MANIFEST_LINK=https://github.com/Project-Fluid/manifest.git
-BRANCH=fluid-12
-ROM_NAME=fluid
+MANIFEST_LINK=https://github.com/Project-Awaken/android_manifest.git
+BRANCH=11
+ROM_NAME=awaken
 DEVICE_CODENAME=vince
 GITHUB_USER=rk134
 GITHUB_EMAIL=rahul.kantrapally@gmail.com
 WORK_DIR=$(pwd)/${ROM_NAME}
-JOBS=nproc
+JOBS=8
 
 # Set up git!
 git config --global user.name ${GITHUB_USER}
@@ -25,9 +25,9 @@ repo sync --current-branch --force-sync --no-clone-bundle --no-tags --optimized-
 rm -rf hardware/qcom-caf/msm8996/audio
 rm -rf hardware/qcom-caf/msm8996/display
 rm -rf hardware/qcom-caf/msm8996/media
-git clone -b A12-fluid-WIP https://github.com/rxhulkxnt44/device_xiaomi_vince.git device/xiaomi/vince
-git clone -b arrow https://github.com/rxhulkxnt44/vendor_xiaomi_vince.git device/xiaomi/vince
-git clone -b arrow https://github.com/rxhulkxnt44/vendor_xiaomi_vince.git vendor/xiaomi/vince
+git clone -b awaken https://github.com/rxhulkxnt44/device_xiaomi_vince.git device/xiaomi/vince
+git clone -b arrow-11.0 https://github.com/rxhulkxnt44/vendor_xiaomi_vince.git vendor/xiaomi/vince
+git clone -b darky https://github.com/Blacksuan19/kernel_dark_ages_vince.git kernel/xiaomi/vince
 cd hardware/qcom-caf/msm8996
 git clone -b eleven https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_display.git hardware/qcom-caf/msm8996/display
 git clone -b eleven https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_audio.git hardware/qcom-caf/msm8996/audio
@@ -37,6 +37,6 @@ cd ../../../
 
 # Start building!
 . build/envsetup.sh
-lunch fluid_${DEVICE_CODENAME}-eng
+lunch awaken_${DEVICE_CODENAME}-eng
 make sepolicy
 
